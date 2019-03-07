229d0c194de7229aaa2a212ee31a29ec95f173ec
SKing <secret.king.1996@gmail.com>
2019-03-08 00:34:07 +0600

    header update 2.0

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,19 +1,33 @@
-$(document).ready(function(){
-	$('.header_bottomMenu__dropdown').hover (
-	 function() {
+//Выпадающее меню
+$(document).ready(function(){ 
+	$('.header_bottomMenu__dropdown').on( 'click', function() {
 		$('.header_blur').toggleClass('active');
 		$('.header_bottomMenu__dropdown__menu').toggleClass('open');
 	});
-
+// Вывод окна об отсутствии результатов###################################
+	// $('.header_bottomMenu__searchForm__textArea').focus(
+	//  function() {
+	// 	$('.header_bottomMenu__searchForm__noSearchResult').addClass('visible');
+	// 	$('.header_blur').addClass('active');
+	// });
+// Скрытие окна об отсутствии результатов###################################
+	// $('.header_bottomMenu__searchForm__textArea').focusout(
+	//  function() {
+	// 	$('.header_bottomMenu__searchForm__noSearchResult').removeClass('visible');
+	// 	$('.header_blur').removeClass('active');
+	// });
+	// ####################################################
+	// Вывод окна результатов###################################
 	$('.header_bottomMenu__searchForm__textArea').focus(
 	 function() {
-		$('.header_bottomMenu__searchForm__noResult').addClass('visible');
+		$('.header_bottomMenu__searchForm__searchResults').addClass('visible');
 		$('.header_blur').addClass('active');
 	});
-
+// Скрытие окна результатов###################################
 	$('.header_bottomMenu__searchForm__textArea').focusout(
 	 function() {
-		$('.header_bottomMenu__searchForm__noResult').removeClass('visible');
+		$('.header_bottomMenu__searchForm__searchResults').removeClass('visible');
 		$('.header_blur').removeClass('active');
 	});
+	// ####################################################
 });
